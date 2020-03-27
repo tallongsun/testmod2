860128dab764760ba47e1d2f8f0764c0e374c1c6
sun.xin <sun.xin@linecorp.com>
2020-03-27 17:46:44 +0800

    xx

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tallongsun/testmod2
+
+go 1.13
+
+require github.com/tallongsun/testmod v0.0.0-20200327090432-8cac8e9beead
---
+++ main.go
@@ -0,0 +1,11 @@
+package  main
+
+
+import (
+  "fmt"
+  "github.com/tallongsun/testmod/hi"
+)
+
+func main() {
+  fmt.Println(hi.Hi("x"))
+}
